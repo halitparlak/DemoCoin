a13be3c04ff4528aadc30a9530ade1f71573fa30
Halit Parlak <halitprlk@hotmail.com>
2023-12-20 00:41:23 +0300

    bazı yazılar silindi

--- DemoCoinPHP/kayit.php
+++ DemoCoinPHP/kayit.php
@@ -176,15 +176,7 @@ License: For each use you must have a valid license purchased only from above li
 				</div>
 				<!--end::Content-->
 				<!--begin::Footer-->
-				<div class="d-flex flex-center flex-column-auto p-10">
-					<!--begin::Links-->
-					<div class="d-flex align-items-center fw-bold fs-6">
-						<a href="#" class="text-muted text-hover-primary px-2">About</a>
-						<a href="#" class="text-muted text-hover-primary px-2">Contact</a>
-						<a href="#" class="text-muted text-hover-primary px-2">Contact Us</a>
-					</div>
-					<!--end::Links-->
-				</div>
+				
 				<!--end::Footer-->
 			</div>
 			<!--end::Authentication - Sign-up-->
