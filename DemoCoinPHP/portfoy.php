42c7197d517a2e7439ee6351d7e7a42a360b71b1
Mustafa Pehlivan <pehlivanmustafa757@gmail.com>
2024-01-09 14:01:32 +0300

    demobot
    demo-bot

--- DemoCoinPHP/portfoy.php
+++ DemoCoinPHP/portfoy.php
@@ -62,6 +62,27 @@
 
     <!-- Anasayfaya dön butonu -->
     <a href="anasayfa.php">Anasayfa</a>
+    <script src="https://cdn.botpress.cloud/webchat/v1/inject.js"></script>
+<script>
+  window.botpressWebChat.init({
+      "composerPlaceholder": "Chat with demo-bot",
+      "botConversationDescription": "Sizlere yardımcı olabilmek için buradayım:)",
+      "botId": "4cd7cf9e-53c3-44b8-9d4c-1b7a70152eab",
+      "hostUrl": "https://cdn.botpress.cloud/webchat/v1",
+      "messagingUrl": "https://messaging.botpress.cloud",
+      "clientId": "4cd7cf9e-53c3-44b8-9d4c-1b7a70152eab",
+      "webhookId": "a895508c-eb12-4c85-81af-12e291f47d03",
+      "lazySocket": true,
+      "themeName": "prism",
+      "botName": "demo-bot",
+      "avatarUrl": "https://d1muf25xaso8hp.cloudfront.net/https%3A%2F%2Fmeta-q.cdn.bubble.io%2Ff1672952221146x417310664985390140%2FChatbot.png?w=&h=&auto=compress&dpr=1&fit=max",
+      "frontendVersion": "v1",
+      "useSessionStorage": true,
+      "enableConversationDeletion": true,
+      "theme": "prism",
+      "themeColor": "#2563eb"
+  });
+</script>
 </body>
 
 </html>
